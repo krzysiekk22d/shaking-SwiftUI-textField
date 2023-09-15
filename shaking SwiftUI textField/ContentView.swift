4df34e2474d58ea73d7c76453d18bac4a32f9c30
krzysiekk22d <klas.club@gmail.com>
2023-09-15 22:03:14 +0200

    Initial Commit

---
+++ shaking SwiftUI textField/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  shaking SwiftUI textField
+//
+//  Created by Krzysztof Czura on 15/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var text: String = ""
+    @State private var isShaking: Bool = false
+
+    var body: some View {
+        ZStack {
+            Color.mint.ignoresSafeArea()
+            VStack {
+                TextField("Enter your text", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.center)
+                    .modifier(ShakeEffect(animatableData: isShaking ? 3 : 0))
+                    .font(Font.system(size: 14))
+                    .padding(.leading, 10)
+                
+                Button("Shake") {
+                    withAnimation {
+                        isShaking.toggle()
+                    }
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ShakeEffect: GeometryEffect {
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let xOffset = sin(animatableData * .pi * 2) * 5 // Wartość 5 określa amplitudę potrząsania.
+        return ProjectionTransform(CGAffineTransform(translationX: xOffset, y: 0))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shaking SwiftUI textField/shaking_SwiftUI_textFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaking_SwiftUI_textFieldApp.swift
+//  shaking SwiftUI textField
+//
+//  Created by Krzysztof Czura on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct shaking_SwiftUI_textFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
